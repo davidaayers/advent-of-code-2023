d95c5171b1ed4770770c94508ba63274f6602ed3
David Ayers <davidaayers@gmail.com>
2023-12-09 07:01:11 -0700

    feat: Day 09 prep

---
+++ day09/day9_tests.py
@@ -0,0 +1,12 @@
+import day9_funcs
+import unittest
+
+test_input = """
+"""
+
+test_input_lines = test_input.split("\n")
+del test_input_lines[0]
+
+class TestDayNine(unittest.TestCase):
+
+    def test_parsing(self):
