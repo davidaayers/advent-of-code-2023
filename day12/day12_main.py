a7299ca698b3b539796e4e3489315b8d131a5725
David Ayers <davidaayers@gmail.com>
2023-12-11 11:12:45 -0600

    feat: Prep for day 12

---
+++ day12/day12_main.py
@@ -0,0 +1,11 @@
+import day12_funcs
+import utils.util as util
+
+input_lines = util.read_file("input.txt")
+
+part_1_answer = 0
+print(f"Part 1: {part_1_answer}")
+
+# Part 2
+part_2_answer = 0
+print(f"Part 2: {part_2_answer}")
---
+++ day12/day12_tests.py
@@ -0,0 +1,14 @@
+import day12_funcs
+import unittest
+
+test_input = """
+"""
+
+test_input_lines = test_input.split("\n")
+del test_input_lines[0]
+
+
+class TestDayTwelve(unittest.TestCase):
+
+    def test_parsing(self):
+
